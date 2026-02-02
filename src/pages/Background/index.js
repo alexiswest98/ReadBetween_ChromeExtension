ee8eb3d1c2a64d3f7669ba206fb5b85275f3b621
Alexis W <alexisw@MacBook-Pro.local>
2026-02-02 14:41:07 -0800

    changed auto load to side panel

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,7 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+// Open side panel when extension icon is clicked
+chrome.action.onClicked.addListener((tab) => {
+  chrome.sidePanel.open({ windowId: tab.windowId });
+});
