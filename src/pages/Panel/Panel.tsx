ade315380470644f59ff9b288fc95d74fe9dacb7
Alexis W <alexisw@MacBook-Pro.local>
2026-02-06 20:24:04 -0800

    updated assets

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Panel.css';
+import logo from '../../assets/img/ReadBetweenLogo_White.png';
 
 const Panel: React.FC = () => {
   return (
     <div className="container">
-      <h1>Read Between Panel</h1>
+      {/* <h1>Read Between Panel</h1> */}
+      <img src={logo} alt="Read Between Logo" className="primary-logo" />
     </div>
   );
 };
